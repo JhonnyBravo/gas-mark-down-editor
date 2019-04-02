0db55f3c939ef1b72cf699c914ff6fd8a7b23c06
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-04-02 22:14:28 +0900

    renamed: Table.js -> src/main/ts/Table.ts
    clasp と TypeScript を導入。

---
+++ src/main/ts/Table.ts
@@ -0,0 +1,121 @@
+class Table extends QueryParameters {
+    private _url: string;
+    private _workbook: GoogleAppsScript.Spreadsheet.Spreadsheet;
+    private _sheet: GoogleAppsScript.Spreadsheet.Sheet;
+
+    constructor(url: string) {
+        super();
+        this._url = url;
+        this._workbook = SpreadsheetApp.openByUrl(this._url);
+    }
+
+    public openSheet(sheetName: string): void {
+        this._sheet = this._workbook.getSheetByName(sheetName);
+
+        if (this._sheet == null) {
+            // 指定したシートが存在しない場合は新規作成する。
+            this._sheet = this._workbook.insertSheet(sheetName);
+            this._sheet.getRange("A1").setValue("id");
+            this._sheet.getRange("B1").setValue("title");
+            this._sheet.getRange("C1").setValue("content");
+        }
+    }
+
+    public findAll(): Object[][] {
+        let row = this._sheet.getRange("A2").getRow();
+        const col = this._sheet.getRange("A2").getColumn();
+        let endRow: string;
+
+        // 値が入力されていない行を検索する。
+        if (this._sheet.getRange(row, col).getValue() == "") {
+            endRow = "A2";
+        } else {
+            while (this._sheet.getRange(row, col).getValue() != "") {
+                row++;
+            }
+
+            endRow = this._sheet.getRange(row - 1, col).getA1Notation();
+        }
+
+        const endCol = "C2";
+        const table = this._sheet.getRange(endRow + ":" + endCol).getValues();
+        return table;
+    }
+
+    public findById(id: number): {} {
+        let row = this._sheet.getRange("A2").getRow();
+        const col = this._sheet.getRange("A2").getColumn();
+
+        while (this._sheet.getRange(row, col).getValue() != "") {
+            if (parseInt(this._sheet.getRange(row, col).getValue().toString()) == id) {
+                break;
+            } else {
+                row++;
+            }
+        }
+
+        const endRow = this._sheet.getRange(row, col).getA1Notation();
+        const endCol = this._sheet.getRange(row, col + 2).getA1Notation();
+        const table = this._sheet.getRange(endRow + ":" + endCol).getValues();
+        return table;
+    }
+
+    public insertRecord(objForm): void {
+        let id: number;
+        const title = objForm.title;
+        const contents = objForm.contents;
+
+        let row = this._sheet.getRange("A2").getRow();
+        const col = this._sheet.getRange("A2").getColumn();
+
+        while (this._sheet.getRange(row, col).getValue() != "") {
+            row++;
+        }
+
+        if (this._sheet.getRange(row, col).getA1Notation() == "A2") {
+            id = 1;
+        } else {
+            id = parseInt(this._sheet.getRange(row - 1, col).getValue().toString()) + 1;
+        }
+
+        this._sheet.getRange(row, col).setValue(id);
+        this._sheet.getRange(row, col + 1).setValue(title);
+        this._sheet.getRange(row, col + 2).setValue(contents);
+    }
+
+    public updateRecord(objForm) {
+        const id = objForm.id;
+        const title = objForm.title;
+        const contents = objForm.contents;
+
+        let row = this._sheet.getRange("A2").getRow();
+        const col = this._sheet.getRange("A2").getColumn();
+
+        while (this._sheet.getRange(row, col).getValue() != "") {
+            if (this._sheet.getRange(row, col).getValue() == id) {
+                this._sheet.getRange(row, col).setValue(id);
+                this._sheet.getRange(row, col + 1).setValue(title);
+                this._sheet.getRange(row, col + 2).setValue(contents);
+                break;
+            } else {
+                row++;
+            }
+        }
+    }
+
+    public deleteRecord(objForm) {
+        const id = objForm.id;
+
+        let row = this._sheet.getRange("A2").getRow();
+        const col = this._sheet.getRange("A2").getColumn();
+
+        while (this._sheet.getRange(row, col).getValue() != "") {
+            if (this._sheet.getRange(row, col).getValue() == id) {
+                this._sheet.deleteRow(row);
+                break;
+            } else {
+                row++;
+            }
+        }
+    }
+}
